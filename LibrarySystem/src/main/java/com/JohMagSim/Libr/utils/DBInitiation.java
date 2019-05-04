6908a88de9cfa9e003b637e5cd13ed990547fc01
Simon Blomsterlund <simon@simonblund.com>
2019-05-04 10:14:18 +0300

    DB working, added users, usertyoe and staff table creators. Also started testing for that class

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/utils/DBInitiation.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/utils/DBInitiation.java
@@ -1,63 +1,35 @@
 package com.JohMagSim.Libr.utils;
+/**
+ * This class is used to create tables in the library sqlite database.
+ * @Author SimonBlund
+ */
+
 
 import java.sql.*;
 
 public class DBInitiation {
-    private Connection connection = null;
+    public static final String url = "jdbc:sqlite:library.db";
 
-    private void createConnection(){
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:library.db");
-            System.out.println("DB created");
-        }
-        catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    private Connection getConnection(){
-        if(connection == null){
-            createConnection();
-            return connection;
-        }
-        else {
-            return connection;
-        }
-    }
-
-    private void closeConnection(Connection conn) {
-        try {
-            if (conn != null) {
-                conn.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void createUsersTable(){
+    /**
+     * Is used to create the tables, takes in sql for the table creation and
+     * table name for error logging.
+     * @param sql String SQL to create the table.
+     * @param table String tablename used for error logging.
+     */
+    private static void createTable(String sql, String table){
         Connection conn = null;
         Statement stmt = null;
-        String sql = "CREATE TABLE IF NOT EXISTS users (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	fName text NOT NULL,\n"
-                + "	lName text NOT NULL,\n"
-                + "	email text NOT NULL UNIQUE,\n"
-                + "	passwordHash text NOT NULL,\n"
-                + "	passwordResetToken text NOT NULL,\n"
-                + "	userTypeID integer DEFAULT 1,\n"
-                + "	staffID integer\n"
-                + ");";
+
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:library.db");
-            System.out.println("DB connected");
+            conn = DriverManager.getConnection(url);
+            System.out.println("DB connected for creation of "+table);
             stmt = conn.createStatement();
             stmt.execute(sql);
         }
         catch (SQLException e)
         {
-            System.out.println(e.getMessage());
+            System.out.println("In creation of table "+table+" this occured "+e.getMessage());
         } finally {
             try {
                 if(stmt != null){
@@ -67,12 +39,44 @@ public class DBInitiation {
                     conn.close();
                 }
             } catch (Exception e){
-                e.printStackTrace();
+                System.out.println("In closing of connection creating  of table "+table+" this occured "+e.getMessage());
             }
         }
     }
+
+    /**
+     * This method runs the initialisation processes for the database.
+     * If new tables are needed just add their create statements here.
+     */
     public static void createTables() {
-        createUsersTable();
+
+        // Users table
+        createTable("CREATE TABLE IF NOT EXISTS users (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	fName text NOT NULL,\n"
+                + "	lName text NOT NULL,\n"
+                + "	email text NOT NULL UNIQUE,\n"
+                + "	passwordHash text NOT NULL,\n"
+                + "	passwordResetToken text NOT NULL,\n"
+                + "	userTypeID integer DEFAULT 1,\n"
+                + "	staffID integer,\n"
+                + "	FOREIGN KEY (staffID) REFERENCES staff(id) ON DELETE CASCADE \n"
+                + ");", "users");
+
+        // UserType table
+        createTable("CREATE TABLE IF NOT EXISTS usertype (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	description text NOT NULL,\n"
+                + "	booksAtATime integer NOT NULL\n"
+                + ");", "userType");
+
+        // Staff table
+        createTable("CREATE TABLE IF NOT EXISTS staff (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	manager integer DEFAULT 0,\n"
+                + "	booksAtATime integer NOT NULL\n"
+                + ");", "staff");
+
     }
 
 }
---
+++ LibrarySystem/src/test/java/com/JohMagSim/Libr/utilsTests/DBInitiationTest.java
@@ -0,0 +1,14 @@
+package com.JohMagSim.Libr.utilsTests;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class DBInitiationTest {
+
+    @Test
+    public void createTablesTest(){
+
+        //assertEquals()
+    }
+}
