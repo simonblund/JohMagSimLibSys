89594f7082b6df98d63c4514ffc410d6c98b95af
JohannaAmiona <johanna.nordmark@outlook.com>
2019-05-16 10:43:51 +0200

    copy.java updated

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/model/Copy.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/model/Copy.java
@@ -1,20 +1,17 @@
 package com.JohMagSim.Libr.model;
 
 public class Copy {
+
+    //skapar items i DB
     private int itemID;
-    private String title;
     private int barCode;
-    private String location;
-    private String classification;
     private int state; //Enum? Skulle kunna vara Enum i .java
     // och int i SQLLite då SQLLite inte kan hantera Enum
 
-    public Copy(int itemID, String title, int barCode, String location, String classification, int state, boolean onLoan) {
+
+    public Copy(int itemID, int barCode, int state) {
         this.itemID = itemID;
-        this.title = title;
         this.barCode = barCode;
-        this.location = location;
-        this.classification = classification;
         this.state = state;
     }
 
@@ -26,36 +23,12 @@ public class Copy {
         this.itemID = itemID;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public int getBarCode() {
         return barCode;
     }
 
     public void setBarCode(int barCode) {
         this.barCode = barCode;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getClassification() {
-        return classification;
-    }
-
-    public void setClassification(String classification) {
-        this.classification = classification;
     }
 
     public int getState() {
@@ -70,10 +43,7 @@ public class Copy {
     public String toString() {
         return "Copy{" +
                 "itemID=" + itemID +
-                ", title='" + title + '\'' +
                 ", barCode=" + barCode +
-                ", location='" + location + '\'' +
-                ", classification='" + classification + '\'' +
                 ", state=" + state +
                 '}';
     }
