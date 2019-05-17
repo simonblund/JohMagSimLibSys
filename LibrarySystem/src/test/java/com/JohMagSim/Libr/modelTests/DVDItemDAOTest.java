0a51e6a4043f4bc61ac665b5b6d6ad1d9d5c12e0
JohannaAmiona <johanna.nordmark@outlook.com>
2019-05-17 14:22:32 +0200

    DVDItemDAOtest created

---
+++ LibrarySystem/src/test/java/com/JohMagSim/Libr/modelTests/DVDItemDAOTest.java
@@ -0,0 +1,43 @@
+package com.JohMagSim.Libr.modelTests;
+
+import org.junit.Test;
+import com.JohMagSim.Libr.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DVDItemDAOTest {
+
+    public DVDItem testDVDitem1() {
+        DVDItem dvdItem = new DVDItem("En film", "A1", "DVD", 1,
+                2016, 1, 14, "12121212", "Drama", "Sweden", 15, testcreateTestActors());
+        // dvdItem.setActors(createTestActors());
+        return dvdItem;
+    }
+
+    public List<String> testcreateTestActors() {
+        ArrayList<String> actors = new ArrayList<>();
+        actors.add("Dustin Hoffman");
+        actors.add("Joaqin Phoenix");
+        return actors;
+    }
+
+    @Test
+    public void itemSaveTest() {
+        DVDItem test = testDVDitem1();
+        DVDItemDAO.createDVDItem(test);
+    }
+
+    @Test
+    public void actorSaveTest() {
+        ArrayList actors = (ArrayList) testcreateTestActors();
+        DVDItemDAO.createDVDActor(actors);
+
+    }
+    }
+
+
+
+
