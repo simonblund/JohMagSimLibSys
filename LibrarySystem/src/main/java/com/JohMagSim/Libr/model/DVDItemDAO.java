5852bad790e30656e5970d8c9fea5e8469d3704b
JohannaAmiona <johanna.nordmark@outlook.com>
2019-05-21 15:37:33 +0200

    DVDItemDAO updated with finding methods.

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/model/DVDItemDAO.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/model/DVDItemDAO.java
@@ -6,14 +6,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class DVDItemDAO {
-    //findDVDbyActor
-    //findDVDbytitle
-    //findORsökning OR-sökning på ej ISBN och ID
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -58,7 +56,9 @@ public class DVDItemDAO {
         return key;
     }
 
-    public static DVDItem findDVDbyId(int id) {
+    public static DVDItem findDVDbyId(int id)
+    //throws Exception
+    {
         Connection conn = DBConnection.getConnection();
         String sql = "SELECT * FROM item WHERE id = ? ;";
         ResultSet rs;
@@ -177,6 +177,159 @@ public class DVDItemDAO {
 
     }
 
+    //Find item from actor
+    public static ArrayList<DVDItem> findDVDFromActor(String actor)
+    //throws Exception
+    {
+        Connection conn = DBConnection.getConnection();
+        String sql = "SELECT * FROM item WHERE actors LIKE ? AND itemType=?;";
+        ResultSet rs = null;
+        ArrayList<DVDItem> result = new ArrayList<DVDItem>();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "%" + actor + "%");
+            pstmt.setString(2, "DVD");
+            rs = pstmt.executeQuery();
+
+            if (!rs.next()) { //om inget resultat
+                new Exception("No actor found");
+            } else {
+                do {
+                    DVDItem dvdItem = new DVDItem();
+                    dvdItem.setId(rs.getInt("id"));
+                    dvdItem.setISBNEAN(rs.getString("ISBN_EAN"));
+                    dvdItem.setTitle(rs.getString("title"));
+                    dvdItem.setType(rs.getString("itemType"));
+                    dvdItem.setEdition(rs.getInt("edition"));
+                    dvdItem.setYear(rs.getInt("year"));
+                    dvdItem.setAgeRestriction(rs.getInt("age_restriction"));
+                    dvdItem.setProdCountry(rs.getString("prod_country"));
+                    dvdItem.setCategory(rs.getString("category"));
+                    dvdItem.setLocation(rs.getString("location"));
+
+                    String actorsFromDB = rs.getString("actors"); //actors as String
+                    String[] split = actorsFromDB.split(","); //common array created by split
+                    dvdItem.setActors(Arrays.asList(split)); //Change from array to list
+
+                    dvdItem.setLoantime(rs.getInt("maximumLoanTime"));
+
+                    result.add(dvdItem);
+                } while (rs.next());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.severe("findDVDItemFromActor " + e.getMessage());
+        } finally {
+            DBConnection.closeConnection(conn);
+        }
+        return result;
+    }
+
+
+    //Find item from actor
+    public static ArrayList<DVDItem> findDVDFromTitle(String title)
+    //throws Exception
+    {
+        Connection conn = DBConnection.getConnection();
+        String sql = "SELECT * FROM item WHERE title LIKE ? AND itemType=?;";
+        ResultSet rs = null;
+        ArrayList<DVDItem> result = new ArrayList<DVDItem>();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "%" + title + "%");
+            pstmt.setString(2, "DVD");
+            rs = pstmt.executeQuery();
+
+            if (!rs.next()) { //om inget resultat
+                new Exception("No DVD found");
+            } else {
+                do {
+                    DVDItem dvdItem = new DVDItem();
+                    dvdItem.setId(rs.getInt("id"));
+                    dvdItem.setISBNEAN(rs.getString("ISBN_EAN"));
+                    dvdItem.setTitle(rs.getString("title"));
+                    dvdItem.setType(rs.getString("itemType"));
+                    dvdItem.setEdition(rs.getInt("edition"));
+                    dvdItem.setYear(rs.getInt("year"));
+                    dvdItem.setAgeRestriction(rs.getInt("age_restriction"));
+                    dvdItem.setProdCountry(rs.getString("prod_country"));
+                    dvdItem.setCategory(rs.getString("category"));
+                    dvdItem.setLocation(rs.getString("location"));
+
+                    String actorsFromDB = rs.getString("actors"); //actors as String
+                    String[] split = actorsFromDB.split(","); //common array created by split
+                    dvdItem.setActors(Arrays.asList(split)); //Change from array to list
+
+                    dvdItem.setLoantime(rs.getInt("maximumLoanTime"));
+
+                    result.add(dvdItem);
+                } while (rs.next());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.severe("findDVDItemFromTitle " + e.getMessage());
+        } finally {
+            DBConnection.closeConnection(conn);
+        }
+        return result;
+    }
+
+
+    //Find item from actor
+    public static ArrayList<DVDItem> findDVDFromTitleActorCountyAgeCategory
+    (String title, String actor, String county, int age, String category)
+    //throws Exception
+    {
+        Connection conn = DBConnection.getConnection();
+        String sql = "SELECT * FROM item WHERE title LIKE ? AND actors LIKE ? " +
+                "AND prod_country LIKE ? " +
+                "AND category LIKE ? AND age_restriction=? AND itemType=?;";
+
+        ResultSet rs = null;
+        ArrayList<DVDItem> result = new ArrayList<DVDItem>();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "%" + title + "%");
+            pstmt.setString(2, "%" + actor + "%");
+            pstmt.setString(3, "%" + county + "%");
+            pstmt.setString(4, "%" + category + "%");
+            pstmt.setInt(5, age);
+            pstmt.setString(6, "DVD");
+            rs = pstmt.executeQuery();
+
+            if (!rs.next()) { //om inget resultat
+                new Exception("No DVD found");
+            } else {
+                do {
+                    DVDItem dvdItem = new DVDItem();
+                    dvdItem.setId(rs.getInt("id"));
+                    dvdItem.setISBNEAN(rs.getString("ISBN_EAN"));
+                    dvdItem.setTitle(rs.getString("title"));
+                    dvdItem.setType(rs.getString("itemType"));
+                    dvdItem.setEdition(rs.getInt("edition"));
+                    dvdItem.setYear(rs.getInt("year"));
+                    dvdItem.setAgeRestriction(rs.getInt("age_restriction"));
+                    dvdItem.setProdCountry(rs.getString("prod_country"));
+                    dvdItem.setCategory(rs.getString("category"));
+                    dvdItem.setLocation(rs.getString("location"));
+
+                    String actorsFromDB = rs.getString("actors"); //actors as String
+                    String[] split = actorsFromDB.split(","); //common array created by split
+                    dvdItem.setActors(Arrays.asList(split)); //Change from array to list
+
+                    dvdItem.setLoantime(rs.getInt("maximumLoanTime"));
+
+                    result.add(dvdItem);
+                } while (rs.next());
+            }
+
+        } catch (SQLException e) {
+            LOGGER.severe("findDVDItemFromTitle " + e.getMessage());
+        } finally {
+            DBConnection.closeConnection(conn);
+        }
+        return result;
+    }
 
 }
 
