c650113c923b070bcd9c83480b488743e460e912
Simon Blomsterlund <simon@simonblund.com>
2019-05-23 21:17:07 +0300

    CreateUser works

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/model/UserTypeRepository.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/model/UserTypeRepository.java
@@ -1,8 +1,36 @@
 package com.JohMagSim.Libr.model;
 
+import java.util.*;
+
 public class UserTypeRepository {
     private static UserType normalUser;
     private static UserType researchUser;
+
+    private static ArrayList<UserType> userTypes = new ArrayList();
+
+    public static ArrayList getUserTypes() {
+        return userTypes;
+    }
+
+    public static void setUserTypes(ArrayList userTypes) {
+        UserTypeRepository.userTypes = userTypes;
+    }
+
+    public static UserType getNormalUser() {
+        return normalUser;
+    }
+
+    public static void setNormalUser(UserType normalUser) {
+        UserTypeRepository.normalUser = normalUser;
+    }
+
+    public static UserType getResearchUser() {
+        return researchUser;
+    }
+
+    public static void setResearchUser(UserType researchUser) {
+        UserTypeRepository.researchUser = researchUser;
+    }
 
     public static void startup(){
         createNormalUserType();
@@ -11,18 +39,20 @@ public class UserTypeRepository {
 
     private static void createNormalUserType(){
         UserType ut = new UserType();
-        ut.setId(1);
+        ut.setId(0);
         ut.setDescription("Normal user");
         ut.setBooksAtATime(5);
         normalUser =ut;
+        userTypes.add(normalUser);
     }
 
     private static void createResearcherUserType(){
         UserType ut = new UserType();
-        ut.setId(2);
+        ut.setId(1);
         ut.setDescription("Research user");
         ut.setBooksAtATime(10);
         researchUser = ut;
+        userTypes.add(researchUser);
     }
 
     public static UserType findUsertype(int id){
--- LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersCreateTabVC.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersCreateTabVC.java
@@ -1,8 +1,12 @@
 package com.JohMagSim.Libr.viewcontroller;
 
+import com.JohMagSim.Libr.controller.*;
+import com.JohMagSim.Libr.model.*;
 import com.JohMagSim.Libr.views.*;
 
+import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class StaffTabUsersCreateTabVC {
     private StaffTabUsersCreateTab cip;
@@ -16,6 +20,14 @@ public class StaffTabUsersCreateTabVC {
     }
 
     public void initStaffTabUsersCreateTab(){
+
+        UserTypeRepository.startup();
+
+        ArrayList<UserType> userTypes = UserTypeRepository.getUserTypes();
+        for(int i=0;i<userTypes.size(); i++){
+            cip.getUsertypeT().addItem(userTypes.get(i).getDescription());
+        }
+        cip.getSaveUser().addActionListener(saveUser);
         cip.setVisible(true);
         cip.updateUI();
     }
@@ -27,9 +39,21 @@ public class StaffTabUsersCreateTabVC {
             String lastname = cip.getLastnameT().getText();
             String email = cip.getEmailT().getText();
             String password = new String(cip.getPasswordT().getPassword());
+            password = AuthControl.hashPassword(password);
+            String passwordReset = AuthControl.getPasswordResetToken();
             int usertypeid = cip.getUsertypeT().getSelectedIndex();
 
-            // Här bryter jag för middag.
+            User user = new User();
+            user.setFirstName(firstname);
+            user.setLastName(lastname);
+            user.setEmail(email);
+            user.setPasswordHash(password);
+            user.setPasswordResetToken(passwordReset);
+            user.setUserTypeID(usertypeid);
+
+            UserDAO.saveUser(user);
+
+            cip.add(new JLabel("User "+firstname+" created."));
 
         }
     };
