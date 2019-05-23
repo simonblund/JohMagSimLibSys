bdc7efea37f5af115f5cf55471e2c0a9f872bf56
Simon Blomsterlund <simon@simonblund.com>
2019-05-23 15:18:55 +0300

    Added createuser form

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersCreateTabVC.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersCreateTabVC.java
@@ -2,6 +2,8 @@ package com.JohMagSim.Libr.viewcontroller;
 
 import com.JohMagSim.Libr.views.*;
 
+import java.awt.event.*;
+
 public class StaffTabUsersCreateTabVC {
     private StaffTabUsersCreateTab cip;
 
@@ -17,4 +19,18 @@ public class StaffTabUsersCreateTabVC {
         cip.setVisible(true);
         cip.updateUI();
     }
+
+    private ActionListener saveUser = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String firstname = cip.getFirstnameT().getText();
+            String lastname = cip.getLastnameT().getText();
+            String email = cip.getEmailT().getText();
+            String password = new String(cip.getPasswordT().getPassword());
+            int usertypeid = cip.getUsertypeT().getSelectedIndex();
+
+            // Här bryter jag för middag.
+
+        }
+    };
 }
--- LibrarySystem/src/main/java/com/JohMagSim/Libr/views/StaffTabUsersCreateTab.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/views/StaffTabUsersCreateTab.java
@@ -13,15 +13,150 @@ public class StaffTabUsersCreateTab extends JComponent {
     private JLabel password = new JLabel("Password");
     private JLabel usertype = new JLabel("Usertype");
 
+    private JTextField firstnameT = new JTextField(10);
+    private JTextField lastnameT = new JTextField(10);
+    private JTextField emailT = new JTextField(10);
+    private JPasswordField passwordT = new JPasswordField(10);
+    private JComboBox usertypeT = new JComboBox();
 
+    private JButton saveUser = new JButton("Save");
+    private JButton clearForm = new JButton("Clear");
 
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public void setLabel(JLabel label) {
+        this.label = label;
+    }
+
+    public JPanel getForm() {
+        return form;
+    }
+
+    public void setForm(JPanel form) {
+        this.form = form;
+    }
+
+    public JLabel getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(JLabel firstname) {
+        this.firstname = firstname;
+    }
+
+    public JLabel getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(JLabel lastname) {
+        this.lastname = lastname;
+    }
+
+    public JLabel getEmail() {
+        return email;
+    }
+
+    public void setEmail(JLabel email) {
+        this.email = email;
+    }
+
+    public JLabel getPassword() {
+        return password;
+    }
+
+    public void setPassword(JLabel password) {
+        this.password = password;
+    }
+
+    public JLabel getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(JLabel usertype) {
+        this.usertype = usertype;
+    }
+
+    public JTextField getFirstnameT() {
+        return firstnameT;
+    }
+
+    public void setFirstnameT(JTextField firstnameT) {
+        this.firstnameT = firstnameT;
+    }
+
+    public JTextField getLastnameT() {
+        return lastnameT;
+    }
+
+    public void setLastnameT(JTextField lastnameT) {
+        this.lastnameT = lastnameT;
+    }
+
+    public JTextField getEmailT() {
+        return emailT;
+    }
+
+    public void setEmailT(JTextField emailT) {
+        this.emailT = emailT;
+    }
+
+    public JPasswordField getPasswordT() {
+        return passwordT;
+    }
+
+    public void setPasswordT(JPasswordField passwordT) {
+        this.passwordT = passwordT;
+    }
+
+    public JComboBox getUsertypeT() {
+        return usertypeT;
+    }
+
+    public void setUsertypeT(JComboBox usertypeT) {
+        this.usertypeT = usertypeT;
+    }
+
+    public JButton getSaveUser() {
+        return saveUser;
+    }
+
+    public void setSaveUser(JButton saveUser) {
+        this.saveUser = saveUser;
+    }
+
+    public JButton getClearForm() {
+        return clearForm;
+    }
+
+    public void setClearForm(JButton clearForm) {
+        this.clearForm = clearForm;
+    }
 
     public StaffTabUsersCreateTab(){
         setLayout(new BorderLayout());
         add(BorderLayout.NORTH, label);
         add(BorderLayout.CENTER,form);
 
-        form.setLayout(new SpringLayout());
-        add(firstname,SpringLayout.NORTH);
+
+        firstname.setLabelFor(firstnameT);
+        form.add(firstname);
+        form.add(firstnameT);
+
+        form.add(lastname);
+        form.add(lastnameT);
+
+        form.add(email);
+        form.add(emailT);
+
+        form.add(password);
+        form.add(passwordT);
+
+        form.add(usertype);
+        form.add(usertypeT);
+
+        form.add(saveUser);
+        form.add(clearForm);
     }
 }
