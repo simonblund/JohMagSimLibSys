4275b272f58f799b09e6ce4e4e74f44d06e7c688
Simon Blomsterlund <simon@simonblund.com>
2019-05-24 09:20:41 +0300

    Fixed create edit staff

--- LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersStaffEditTabVC.java
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/viewcontroller/StaffTabUsersStaffEditTabVC.java
@@ -7,46 +7,38 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class StaffTabUsersEditTabVC {
-    private StaffTabUsersEditTab cip;
-    private User loadedUser;
+public class StaffTabUsersStaffEditTabVC {
+    private StaffTabUsersStaffEditTab cip;
+    private Staff loadedUser;
     private ArrayList<UserType> userTypes = UserTypeRepository.getUserTypes();
 
-    public StaffTabUsersEditTabVC(StaffTabUsersEditTab editUser){
+    public StaffTabUsersStaffEditTabVC(StaffTabUsersStaffEditTab editUser){
         cip = editUser;
     }
 
-    public StaffTabUsersEditTab getCip(){
+    public StaffTabUsersStaffEditTab getCip(){
         return cip;
     }
 
-    public void initStaffTabUsersEditTab(){
+    public void initStaffTabUsersStaffEditTab(){
         cip.getSearchButton().addActionListener(searchUser);
 
 
         cip.getSearchuser().setVisible(true);
         cip.getSearchButton().setVisible(true);
-        cip.getClearForm().addActionListener(clearForm);
 
         cip.setVisible(true);
         cip.updateUI();
     }
-    private ActionListener clearForm = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            clearFields();
-        }
-    };
 
     private ActionListener searchUser = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             String email = cip.getSearchuser().getText();
             try {
-                User user = UserDAO.findUserFromEmail(email);
+                Staff user = StaffDAO.findStaffFromEmail(email);
                 loadedUser = user;
                 editUser(user);
-
 
             } catch (Exception a){
                 System.out.println(a.getMessage());
@@ -55,7 +47,7 @@ public class StaffTabUsersEditTabVC {
         }
     };
 
-    private void editUser(User user){
+    private void editUser(Staff user){
 
 
         for(int i=0;i<userTypes.size(); i++){
@@ -66,35 +58,49 @@ public class StaffTabUsersEditTabVC {
         cip.getLastnameT().setText(user.getLastName());
         cip.getEmailT().setText(user.getEmail());
         cip.getUsertypeT().setSelectedIndex(user.getUserTypeID());
+        cip.getIsManagerT().setSelected(user.isManager());
         cip.remove(cip.getSearchForm());
         cip.add(BorderLayout.CENTER,cip.getForm());
         cip.getSaveUser().addActionListener(saveUser);
+        cip.getClearForm().addActionListener(clearForm);
         cip.getForm().setVisible(true);
         cip.updateUI();
 
     }
 
+    private ActionListener clearForm = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            clearFields();
+        }
+    };
+
     private ActionListener saveUser = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            User user = loadedUser;
+            Staff user = loadedUser;
 
             String firstname = cip.getFirstnameT().getText();
             String lastname = cip.getLastnameT().getText();
             String email = cip.getEmailT().getText();
             int usertypeid = cip.getUsertypeT().getSelectedIndex();
             int booksAtATime = userTypes.get(usertypeid).getBooksAtATime();
+            boolean isManager = cip.getIsManagerT().isSelected();
 
             user.setFirstName(firstname);
             user.setLastName(lastname);
             user.setEmail(email);
             user.setUserTypeID(usertypeid);
             user.setBooksAtATime(booksAtATime);
+            user.setManager(isManager);
 
-            UserDAO.updateUser(user);
+            StaffDAO.updateUser(user);
+
+            clearFields();
 
         }
     };
+
     private void clearFields(){
         cip.remove(cip.getSearchForm());
         cip.add(cip.getSearchForm());
---
+++ LibrarySystem/src/main/java/com/JohMagSim/Libr/views/StaffTabUsersStaffEditTab.java
@@ -0,0 +1,203 @@
+package com.JohMagSim.Libr.views;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StaffTabUsersStaffEditTab extends JComponent{
+    private JLabel label = new JLabel("Create staff");
+    private JPanel form = new JPanel();
+
+    private JLabel firstname = new JLabel("Firstname");
+    private JLabel lastname = new JLabel("Lastname");
+    private JLabel email = new JLabel("Email");
+
+    private JLabel usertype = new JLabel("Usertype");
+    private JLabel isManager = new JLabel("Is manager");
+
+    private JTextField firstnameT = new JTextField(10);
+    private JTextField lastnameT = new JTextField(10);
+    private JTextField emailT = new JTextField(10);
+
+    private JComboBox usertypeT = new JComboBox();
+    private JCheckBox isManagerT = new JCheckBox();
+
+    private JButton saveUser = new JButton("Save");
+    private JButton clearForm = new JButton("Clear");
+
+    private JPanel searchForm = new JPanel();
+    private JLabel searchEmail = new JLabel("Search user by email");
+    private JTextField searchuser = new JTextField(10);
+    private JButton searchButton = new JButton("Search");
+
+    public JPanel getSearchForm() {
+        return searchForm;
+    }
+
+    public void setSearchForm(JPanel searchForm) {
+        this.searchForm = searchForm;
+    }
+
+    public JTextField getSearchuser() {
+        return searchuser;
+    }
+
+    public void setSearchuser(JTextField searchuser) {
+        this.searchuser = searchuser;
+    }
+
+    public JButton getSearchButton() {
+        return searchButton;
+    }
+
+    public void setSearchButton(JButton searchButton) {
+        this.searchButton = searchButton;
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public void setLabel(JLabel label) {
+        this.label = label;
+    }
+
+    public JPanel getForm() {
+        return form;
+    }
+
+    public void setForm(JPanel form) {
+        this.form = form;
+    }
+
+    public JLabel getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(JLabel firstname) {
+        this.firstname = firstname;
+    }
+
+    public JLabel getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(JLabel lastname) {
+        this.lastname = lastname;
+    }
+
+    public JLabel getEmail() {
+        return email;
+    }
+
+    public void setEmail(JLabel email) {
+        this.email = email;
+    }
+
+    public JLabel getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(JLabel usertype) {
+        this.usertype = usertype;
+    }
+
+    public JTextField getFirstnameT() {
+        return firstnameT;
+    }
+
+    public void setFirstnameT(JTextField firstnameT) {
+        this.firstnameT = firstnameT;
+    }
+
+    public JTextField getLastnameT() {
+        return lastnameT;
+    }
+
+    public void setLastnameT(JTextField lastnameT) {
+        this.lastnameT = lastnameT;
+    }
+
+    public JTextField getEmailT() {
+        return emailT;
+    }
+
+    public void setEmailT(JTextField emailT) {
+        this.emailT = emailT;
+    }
+
+
+    public JComboBox getUsertypeT() {
+        return usertypeT;
+    }
+
+    public void setUsertypeT(JComboBox usertypeT) {
+        this.usertypeT = usertypeT;
+    }
+
+    public JButton getSaveUser() {
+        return saveUser;
+    }
+
+    public void setSaveUser(JButton saveUser) {
+        this.saveUser = saveUser;
+    }
+
+    public JButton getClearForm() {
+        return clearForm;
+    }
+
+    public void setClearForm(JButton clearForm) {
+        this.clearForm = clearForm;
+    }
+
+    public JLabel getIsManager() {
+        return isManager;
+    }
+
+    public void setIsManager(JLabel isManager) {
+        this.isManager = isManager;
+    }
+
+    public JCheckBox getIsManagerT() {
+        return isManagerT;
+    }
+
+    public void setIsManagerT(JCheckBox isManagerT) {
+        this.isManagerT = isManagerT;
+    }
+
+    public StaffTabUsersStaffEditTab(){
+        setLayout(new BorderLayout());
+        add(BorderLayout.NORTH, label);
+        add(BorderLayout.CENTER,form);
+
+        setLayout(new BorderLayout());
+        add(BorderLayout.NORTH, label);
+
+
+        searchForm.add(searchEmail);
+        searchForm.add(searchuser);
+        searchForm.add(searchButton);
+        add(BorderLayout.CENTER, searchForm);
+
+
+        firstname.setLabelFor(firstnameT);
+        form.add(firstname);
+        form.add(firstnameT);
+
+        form.add(lastname);
+        form.add(lastnameT);
+
+        form.add(email);
+        form.add(emailT);
+
+        form.add(usertype);
+        form.add(usertypeT);
+
+        form.add(isManager);
+        form.add(isManagerT);
+
+        form.add(saveUser);
+        form.add(clearForm);
+    }
+}
